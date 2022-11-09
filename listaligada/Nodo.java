04281b1ddb26399022d9b4e90748596a5142f9bd
Leonel-David-Navarro-Meneses <leoneldavidnavarromeneses66@gmail.com>
2022-11-09 09:44:30 -0600

    commit inicial

---
+++ listaligada/Nodo.java
@@ -0,0 +1,30 @@
+package listaligada;
+
+public class Nodo {
+    private int dato;//dato almacenado
+    private Nodo siguiente; //apuntador al siguiente nodo
+
+public Nodo(int d){
+    this.dato = d;
+    this.siguiente = null;
+}
+
+public int getDato() {
+    return dato;
+}
+
+public void setDato(int dato) {
+    this.dato = dato;
+}
+
+public Nodo getSiguiente() {
+    return siguiente;
+}
+
+public void setSiguiente(Nodo otroNodo){
+    this.siguiente = otroNodo;
+}
+
+}
+
+
---
+++ listaligada/TestingListaLigada.java
@@ -0,0 +1,52 @@
+package listaligada;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class TestingListaLigada {
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader bufer = new BufferedReader(new InputStreamReader(System.in));
+        String entrada;
+        int dato;
+
+        char respuesta;
+        ListaLigada lista = new ListaLigada();
+        
+     while(true){//ciclo infinito
+
+        
+
+        System.out.println("escribe un dato entero: ");
+        entrada= bufer.readLine();
+        dato = Integer.parseInt(entrada);
+        lista.add(dato);
+        System.out.println("se ha agregado el dato a la lista");
+        System.out.println("quieres agregar mas datos? (s/n)");
+        entrada = bufer.readLine();
+        respuesta = entrada.charAt(0);
+        if(respuesta == 'n' || respuesta == 'N')
+        break;
+    }
+        
+
+        System.out.println("---------------------------");
+        System.out.println("contenido de la lista ligada");
+        lista.printList();
+
+        System.out.println("--------------------------");
+        System.out.println("insercion de un nuevo nodo");
+        System.out.println("escribe el elemento a insertar");
+        entrada = bufer.readLine();
+        dato = Integer.parseInt(entrada);
+        System.out.println("escribe la posiscion de donde quieres insertar: ");
+       entrada= bufer.readLine();
+       int p = Integer.parseInt(entrada);
+       lista.add(dato, p);
+
+       System.out.println("----------------");
+       System.out.println("contenido de la lista ligada: ");
+       lista.printList();
+    }
+}
